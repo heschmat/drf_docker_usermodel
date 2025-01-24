eaeb97513e60c7ddf2ff990715296bd4e6e4bbef
Hesch <heschmat.seyedi@gmail.com>
2025-01-25 00:54:30 +0100

    Add custom *user* model.

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,38 @@
+"""
+Database models.
+"""
+
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager
+)
+
+
+class UserManager(BaseUserManager):
+    """Custom User Manager."""
+
+    def create_user(self, email, password, **extra_fields):
+        """Create, save & return a new user."""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        # We pass `using=self._db` just in case  in the future we want to
+        # add multiple databases. It's future proofing.
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom User Model for our Project."""
+    email = models.EmailField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    # Define the field used for authentication.
+    USERNAME_FIELD = 'email'
